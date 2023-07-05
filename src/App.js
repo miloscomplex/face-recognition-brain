5f838ba2d7696d367b2a941817821e8b274642da
Michael Sutton <hicallmesutton@gmail.com>
2023-07-05 18:45:31 -0500

    added Clarifai and particles-bg libraries

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import ImageLinkForm from './components/ImageLinkForm';
 import Rank from "./components/Rank";
+import ParticlesBg from "particles-bg";
+import Clarifai from 'clarifai';
+
+// const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'bd721852e03f4d62829f536d1f97d5c8'
+});
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <ParticlesBg color="#444444" num={150} type="cobweb" config={this.config} bg={true} />
         <Navigation />
         <Logo />
         <Rank />
