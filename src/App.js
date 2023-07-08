669845afaf7cbe76b23d9d30fcd033f75933b4e5
Michael Sutton <hicallmesutton@gmail.com>
2023-07-07 22:01:52 -0500

    added form functionality

--- src/App.js
+++ src/App.js
@@ -23,8 +23,25 @@ class App extends Component {
       imageUrl: "", 
       box: {},
       route: 'signin', 
-      isSignIn: false
+      isSignIn: false,
+      user: {
+        id: '',
+        name: '', 
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name, 
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   onInputChange = (event) => {
